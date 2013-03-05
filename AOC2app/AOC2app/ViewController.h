e20b1513a440af71e6fbd6a81aa18db2ab5d9a30
Courtney Ardis <ardiscb@fullsail.edu>
2013-03-04 19:43:41 -0500

    Added initial files.

---
+++ AOC2app/AOC2app/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC2app
+//
+//  Created by Courtney Ardis on 3/4/13.
+//  Copyright (c) 2013 Courtney Ardis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AOC2app/AOC2app/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AOC2app
+//
+//  Created by Courtney Ardis on 3/4/13.
+//  Copyright (c) 2013 Courtney Ardis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
